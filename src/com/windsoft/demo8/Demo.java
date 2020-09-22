0ac6d6ef4735629c02514da76cce400e612e7792
Ricost <ricost@foxmail.com>
2020-09-22 10:16:34 +0800

    多线程

---
+++ src/com/windsoft/demo8/Demo.java
@@ -0,0 +1,22 @@
+package com.windsoft.demo8;
+
+import java.lang.annotation.*;
+
+public class Demo {
+    @MyAnnotation(value = "张三",num = 100)
+    public static void main(String[] args) {
+
+    }
+}
+//是否用在文档中
+@Documented
+//用途类型
+@Target({ElementType.TYPE, ElementType.METHOD})
+//保存策略
+@Retention(RetentionPolicy.RUNTIME)
+//可以继承
+@Inherited
+@interface MyAnnotation{
+    String value();
+    int num();
+}
